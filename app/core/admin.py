f559627157b7f0584729b7902d5ce8f0ef9142b9
Tim Lee <tim.lee@paywiser.com>
2022-12-01 19:31:55 -0800

    Test User API

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None,{'fields':('email', 'password')}),
         (
-            _('Permission'),
+            _('Permissions'),
             {
                 'fields':(
                     'is_active',
@@ -24,8 +24,22 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        (_('Important dates'), {'fields':('last_login')}),
+        (_('Important dates'), {'fields':('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':(
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
