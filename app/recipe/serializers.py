83a7e2d4c963393ff3c77691ec04c28a53f17e1d
Tim Lee <tim.lee@paywiser.com>
2022-12-08 01:50:41 -0800

    Adding Tags APIs v1.3

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,10 @@ Serializers for recipe APIs
 """
 
 from rest_framework import serializers
-from core.models import Recipe, Tag
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag."""
@@ -19,13 +22,11 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'description', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a new Recipe."""
-        tags = validated_data.pop('tags',[])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting of creating tags as needed."""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -34,8 +35,26 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a new Recipe."""
+        tags = validated_data.pop('tags',[])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
 
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags',None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail views."""
