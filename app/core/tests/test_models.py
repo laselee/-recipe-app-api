d83cae1dbffe2506cbe0d0ebfd398c5651686cfe
Tim Lee <tim.lee@paywiser.com>
2022-12-16 22:43:48 -0800

    Deployment configuration

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,7 @@
 """
 Test for models
 """
-
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -80,3 +80,11 @@ class ModleTests(TestCase):
         ingredient = models.Ingredient.objects.create(user=user, name='Ingredient1')
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -72,9 +72,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags',None)
+        ingredients = validated_data.pop('ingredients', None)
+
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
@@ -86,4 +92,13 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail views."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
